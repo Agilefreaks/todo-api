d361c59a3c2cca3284c97f39f8c7de31c0d52359
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-16 15:39:05 +0300

    Create mock todos in json-api format

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var todos = [
+    { id: 1, value: "Walk dog" },
+    { id: 2, value: "Buy milk" },
+    { id: 3, value: "Take out trash" },
+]
+
+var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+
+var TodoSerializer = new JSONAPISerializer('todos', {
+  attributes: ['value']
+});
+
+var todos = TodoSerializer.serialize(todos);
+
 app.get('/v1/status', (req, res) => res.status(204).send(''));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
