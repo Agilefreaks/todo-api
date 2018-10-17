a755af1f476ce566be823cff2facdaef4f863f08
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-17 14:22:21 +0300

    Split tests

---
+++ test/integration/v1/status-test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../../../app');
+
+describe('GET', function() {
+    describe('/v1/status', function() {
+        it('responds with status code 204', function(done) {
+            request(app)
+                .get('/v1/status')
+                .expect(204, done);
+        });
+    });
+});
--- test/integration/v1/todos-test.js
+++ test/integration/v1/todos-test.js
@@ -1,24 +1,17 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../../../app');
 
-var todos = require('../mockup/todos');
+var todos = require('../../../repository/todosRepository');
 
 describe('GET', function() {
-    describe('/v1/status', function() {
-        it('responds with status code 204', function(done) {
-            request(app)
-                .get('/v1/status')
-                .expect(204, done);
-        });
-    });
-
     describe('/v1/todos', function() {
         it('retrieves a list of todos', function(done) {
+            var todosList = todos.getTodos();
             request(app)
                 .get('/v1/todos')
                 .set('Accept', 'application/vnd.api+json')
                 .expect('Content-Type', 'application/vnd.api+json; charset=utf-8')
-                .expect(todos)
+                .expect(todosList)
                 .expect(200, done)
         });
     });
