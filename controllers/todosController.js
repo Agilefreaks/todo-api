566b39912b2cf0543ee23afb69dba0e5d305b90b
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-17 14:06:45 +0300

    Move mockup to repository and serializer to controller

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -1,5 +1,18 @@
+var todosRepository = require('../repository/todosRepository')
 
 exports.getTodos = function (_, res) {
-    res.header('Content-Type', 'application/vnd.api+json');
-    res.send("getTodos");
+  var todos = serialize(todosRepository.getTodos());
+  res.header('Content-Type', 'application/vnd.api+json');
+  res.send(todos);
 }
+
+serialize = function(input) {
+  var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+  
+  var TodoSerializer = new JSONAPISerializer('todos', {
+    attributes: ['text', 'checked']
+  });
+
+  return TodoSerializer.serialize(input);
+}
+  
---
+++ repository/todosRepository.js
@@ -0,0 +1,10 @@
+var todos = [
+    {id: 1, text: 'Buy milk', checked: true},
+    {id: 2, text: 'Walk skunk', checked: true},
+    {id: 3, text: 'Take out trash', checked: true},
+    {id: 4, text: 'World domination', checked: true}
+]
+
+exports.getTodos = function() {
+    return todos;
+  }
